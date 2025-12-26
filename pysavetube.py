a19f65f5c2e8e42e7cdf7318da9e2cb799ff6bc3
princeconch <princeconch@proton.me>
2025-12-26 18:05:56 +1100

    add comments and filtering for mp4s

--- pysavetube.py
+++ pysavetube.py
@@ -8,13 +8,13 @@ import sys
 import ffmpeg
 
 
-#Clear terminal, setup text
+#clear terminal, setup text
 subprocess.run("clear")
 f = pyfiglet.figlet_format("PySaveTube")
 print(colored(f, "yellow"))
 print("\nA simple wrapper for the pytubefix library, utilising FFMpeg and bash.\n")
 
-#User inputs URL and format to save as; while loop for invalid URLs, recognition for playlists
+#user inputs URL and format to save as; while loop for invalid URLs, recognition for playlists
 while True:
     url = input("Enter URL: ")
     try:
@@ -29,47 +29,57 @@ while True:
             print("Invalid URL.")
     else:
         break
+    
 format = input("Format to save video(s) as (must be ffmpeg supported): ")
 
 if isplaylist == False:
 
     print(f"Downloading {yt.title}...")
 
-
+    #download video and audio streams separately for highest quality
     yt.streams.filter(adaptive=True, file_extension="mp4", only_video=True).order_by("resolution").desc().first().download(filename="video.mp4")
     yt.streams.filter(adaptive=True, file_extension="mp4", only_audio=True).order_by("abr").desc().first().download(filename="audio.mp4")
 
-    #Recombine with ffmpeg
+    #recombine with ffmpeg
     subprocess.run(["ffmpeg", "-i", "video.mp4", "-i", "audio.mp4", "-c", "copy", f"{yt.title}.mp4"], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
 
-    #Move file to downloads
+    #move file to downloads
     subprocess.run(["mv", f"{yt.title}.mp4", f"{os.path.expanduser("~")}/Downloads/"])
 
-    #Change file to desired format
-    subprocess.run(["ffmpeg", "-i", f"{os.path.expanduser("~")}/Downloads/{yt.title}.mp4", f"{os.path.expanduser("~")}/Downloads/{yt.title}.{format}"])
+    #change file to desired format (if not mp4)
+    if format != "mp4":
+       subprocess.run(["ffmpeg", "-i", f"{os.path.expanduser("~")}/Downloads/{yt.title}.mp4", f"{os.path.expanduser("~")}/Downloads/{yt.title}.{format}"])
 
-    #Remove original audio and video files
+    #remove original audio and video files
     subprocess.run(["rm", "audio.mp4", "video.mp4", f"{os.path.expanduser("~")}/Downloads/{yt.title}.mp4"], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
 
 else:
 
     print(f"Downloading {pl.title}...")
 
+    #make a directory for the playlist files to be put in
     subprocess.run(["mkdir", f"{pl.title}"])
 
     for video in pl.videos:
 
+        #download video and audio streams separately
         video.streams.filter(adaptive=True, file_extension="mp4", only_video=True).order_by("resolution").desc().first().download(filename="video.mp4")
         video.streams.filter(adaptive=True, file_extension="mp4", only_audio=True).order_by("abr").desc().first().download(filename="audio.mp4")
 
+        #combine files together
         subprocess.run(["ffmpeg", "-i", "video.mp4", "-i", "audio.mp4", "-c", "copy", f"{video.title}.mp4"], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
         
-        subprocess.run(["ffmpeg", "-i", f"{video.title}.mp4", f"{video.title}.{format}"], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+        #change file to desired format (if not mp4)
+        if format != "mp4":
+            subprocess.run(["ffmpeg", "-i", f"{video.title}.mp4", f"{video.title}.{format}"], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
         
+        #remove original audio and video files
         subprocess.run(["rm", "audio.mp4", "video.mp4", f"{video.title}.mp4"], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
         
+        #move file into playlist folder
         subprocess.run(["mv", f"{video.title}.{format}", f"{pl.title}/"], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
     
+    #move playlist folder to downloads folder
     subprocess.run(["mv", f"{pl.title}/", f"{os.path.expanduser("~")}/Downloads/Run 3/"])
 
 print("Downloaded! Please check your downloads folder.")
